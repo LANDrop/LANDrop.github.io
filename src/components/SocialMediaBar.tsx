6946cec36aefbee39933a97c309c70552b59358c
yvbbrjdr <xyyqzy@hotmail.com>
2024-10-12 13:35:58 -0700

    hide bmac when too small

--- src/components/SocialMediaBar.tsx
+++ src/components/SocialMediaBar.tsx
@@ -1,7 +1,7 @@
 function SocialMediaBar() {
   return (
     <div className="flex items-center space-x-2">
-      <a href="https://www.buymeacoffee.com/LANDrop" target="_blank">
+      <a href="https://www.buymeacoffee.com/LANDrop" target="_blank" className="hidden md:block">
         <img src="https://cdn.buymeacoffee.com/buttons/v2/arial-blue.png" className="h-8" />
       </a>
       <a href="https://discord.gg/5adm6sPugT" target="_blank">
